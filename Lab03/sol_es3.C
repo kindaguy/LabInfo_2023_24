39c7ebfda98a8b8c01974bade6f76d9b27e40ada
Dario Tamascelli <dario.tamascelli@unimi.it>
2023-10-17 13:14:28 +0200

    lab03_post

--- Lab03/sol_es3.C
+++ Lab03/sol_es3.C
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+//Dichiarazione di funzione
+int leggiValore(int,int);
 
 int main(){
 
@@ -16,8 +18,9 @@ int main(){
     cout << endl <<  "Inserire valori: " << endl;
 
     //Istruzione di lettura da tastiera
-    cin >> a;
-    cin >> b;
+   //  cin >> a;
+   //  cin >> b;
+   cin >> a >> b;
     //Equivalente cin >> a >> b;
 
     //Stampo il valore letto
@@ -43,15 +46,58 @@ int main(){
 
     //A questo punto ho definito il range
 
-    //Questo blocco di istruzioni filtra l'input
-    //garantendo che dopo il blocco il valore registrato
-    //(in val) sia "corretto".
-     do{
-        //Istruzione di lettura da tastiera
-        cout << endl << "Inserire valore in [ "<< minimo << "..." << massimo <<"]";
-        cin >> val;
-    } while(val<minimo or val>massimo ); //Equivalentemente (a<18 || a>30 )
+
+
+
+   val = leggiValore(minimo,massimo);
+  
+  
+  
+   // do{
+   //    cout << endl << "inserire valore compreso tra" << minimo << " e " << massimo<< endl;
+   //    cin >> val;
+   // }while(val<minimo or val >massimo);
+
+
+
+   // cout << endl << "inserire valore compreso tra" << minimo << " e " << massimo<< endl;
+   // cin >> val;
+   // while(val<minimo or val >massimo){
+   //    cout << endl << "inserire valore compreso tra" << minimo << " e " << massimo<< endl;
+   //    cin >> val;
+   // }
+
+   // cout << endl << "inserire valore compreso tra" << minimo << " e " << massimo<< endl;
+   // cin >> val;
+   // while(val<minimo or val >massimo){
+   //    cout << endl << "HO DETTO: inserire valore compreso tra" << minimo << " e " << massimo<< endl;
+   //    cin >> val;
+   // }
+   
+   //  //Questo blocco di istruzioni filtra l'input
+   //  //garantendo che dopo il blocco il valore registrato
+   //  //(in val) sia "corretto".
+   //   do{
+   //      //Istruzione di lettura da tastiera
+   //      cout << endl << "Inserire valore in [ "<< minimo << "..." << massimo <<"]";
+   //      cin >> val;
+   //  } while(val<minimo or val>massimo ); //Equivalentemente (a<18 || a>30 )
 
     cout << endl <<"Valore valido letto! " << val << endl;
     return 0;
+}
+
+
+//Definizione di funzione
+int leggiValore(int min,int max){
+   
+   int value;
+
+   do{
+      cout << endl << "inserire valore compreso tra" << min << " e " << max<< endl;
+      cin >> value;
+   }while(value<min or value >max);
+
+   return value;
+
 }
